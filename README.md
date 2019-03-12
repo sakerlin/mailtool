0f8c4a6643ef3cb988dc3744247ab490da2e5a87
saker <saker@oath.com>
2019-03-12 15:54:01 +0800

    init package

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# mailtool
+Email template
+-----------
+
+**開發說明**
+* 因為email html content 諸多限制, 為了完整呈現在寄email 時需把 internal Style Sheet 轉為 inline style
+* 這邊用juice 這個package 來做這件事
+* 為了測試方便用 nodemailer 來做寄送mail 的工具
+
+**inline-css.js**
+* 把 juice 與 nodemailer 集合起來的command line tool
+
+* Usage:
+  ```
+  npm link
+  inline-css.js yourEmailHtml.html
+  ```
+* 會在 yourEmailHtml.html path 下 生成 yourEmailHtml_cssInLine.html , yourEmailHtml_cssInLine.html  即為 inline style
+的 yourEmailHtml.html
+* Send mail:
+```
+  inline-css.js yourEmailHtml.html --sendmail
+```
+* 會把 html 轉為 inline style 後寄送 mail 到設定好的信箱
+* 請在 mailList 設定寄送的 mail address
+* 使用 gmail 寄送的話需啟用 IMAP,password 請用 google app password
---
+++ src/inline-css.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+const juice = require('juice');
+const fs = require('fs')
+const path = require('path')
+const nodemailer = require('nodemailer')
+const moment = require('moment')
+
+//宣告發信物件
+// gmail 需啟用 IMAP
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'saker.lin@gmail.com',
+        pass: 'lzrjthtzaozdiwcc'  // google app password
+    }
+})
+
+const uedAndPm = [
+  'cha0729@verizonmedia.com',
+  'sophiak@verizonmedia.com',
+  'tzuchiw@verizonmedia.com',
+  'anadem@verizonmedia.com'
+]
+const mailList = [
+  'saker.lin@gmail.com',
+  'saker.lin@yahoo.com.hk',
+  'saker.lin@aol.com',
+  'saker.lin@icloud.com',
+  'saker.lin@outlook.com',
+  'saker.lin@zoho.com'
+]
+const sourceFile = process.argv[2]
+const sendMail = (process.argv.indexOf('--sendMail') !== -1)
+
+if (fs.existsSync(sourceFile)) {
+
+  var isForSender = sourceFile.indexOf('voucherEmailForSender') !== -1
+  const fileName = path.basename(sourceFile).split('.')
+  const dirName = path.dirname(sourceFile)
+  const source = fs.readFileSync(sourceFile, 'utf8')
+  const result = juice(source, {preserveMediaQueries: true})
+  if (sendMail) {
+    let i = 0
+    mailList.map((targetMail) => {
+      i++
+      setTimeout(() => {
+        const options = {
+            from: 'kelyn.guo@gmail.com', //寄件者
+            to: targetMail, //收件者
+            cc: '', //副本
+            bcc: '', //密件副本
+            subject: `TEST - eVoucherEmail for ${isForSender ? '寄件者' : '收件者' } ${moment().format('MM/DD HH:ss')} 請幫忙驗證`, // Subject line
+            text: 'eVoucherEmail', // plaintext body
+            html: result,
+            attachments: []
+        }
+
+        //發送信件
+        transporter.sendMail(options, function(error, info) {
+            if(error){
+                console.log(error);
+            } else {
+                console.log(`${moment().format('HH:ss')} 郵件發送 >>>>>>> ${targetMail}`);
+                console.log(`${moment().format('HH:ss')} 郵件標題 >>>>>>> `+ options.subject);
+                console.log(`${moment().format('HH:ss')} 發送結果 >>>>>>> ${info.response}`);
+                console.log('');
+            }
+        })
+      }, 1000 + i*1000 )
+     })
+  }
+  //fs.writeFileSync(`${dirName}/${fileName[0]}_cssInLine.html`, result, 'utf8');
+  fs.writeFile(`${dirName}/${fileName[0]}_cssInLine.html`, result, 'utf-8', (err) => {
+      if (err) {
+        console.log("failed to save");
+      } else {
+        console.log(`Conver result is saving to ${dirName}/${fileName[0]}_cssInLine.html`);
+      }
+  })
+} else {
+  console.log('Source File not found')
+  console.log('USAGE: inline-css [fileName]')
+}
